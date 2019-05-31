0046ac18a617829d4c60fd85b4481b6fd7787618
EC2 Default User <ec2-user@ip-172-31-47-91.us-east-2.compute.internal>
2019-05-31 11:22:10 +0000

    Make a basic User model (including secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,3 +16,16 @@ test "password should be present (nonblank)" do
     assert_not @user.valid?
   end
 end
+test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
